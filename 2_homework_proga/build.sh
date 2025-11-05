3e9116b1b9f205b35cfea9f6a7e5fed8a283db4d
Mo1ecu1a <puazhue@gmail.com>
2025-11-05 17:21:11 +0300

    Improve build.sh to create exe in bin folder

--- 2_homework_proga/build.sh
+++ 2_homework_proga/build.sh
@@ -1,9 +1,8 @@
-MAIN=main.cpp
-APP=2_homework_proga
+FILES="main.cpp array_utils.cpp sort_algorithms.cpp user_interface.cpp"
+APP="bin/2_homework_proga"
 
-if [ -f $APP ]; then rm $APP
-fi
+mkdir -p bin
 
-g++ $MAIN array_utils.cpp sort_algorithms.cpp user_interface.cpp -o $APP
+g++ $FILES -o $APP
 
 ./$APP
